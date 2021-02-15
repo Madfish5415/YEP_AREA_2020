fc9a1f2f95babef3f8f39a8ac40142946d69c98a
mattéo <fauchonmatteo@gmail.com>
2021-02-15 11:09:25 +0100

    feat: WIP add workflow items without link it with the blocs

--- mobile/src/components/workflows/workflows.tsx
+++ mobile/src/components/workflows/workflows.tsx
@@ -1,9 +1,22 @@
 import React, { FC } from "react";
 import { Text, useTheme } from "react-native-paper";
+import { WorkflowItem } from "./workflow-item";
+import { View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const WorkflowsScreen: FC = () => {
-  const { fonts } = useTheme();
-  return <Text style={fonts.nodeLabel}>Workflows</Text>;
+  return (
+    <View style={styles.container}>
+      <WorkflowItem label={"Workflow 1"} isActive={true} />
+      <WorkflowItem label={"Workflow 1"} isActive={false} />
+      <WorkflowItem label={"Workflow 1"} isActive={true} />
+    </View>
+  );
 };
 
 export default WorkflowsScreen;
