a20ced41ce3e80443e1a4288458a31a9b6df5dad
Mattéo Fauchon <46529475+Matttx@users.noreply.github.com>
2021-02-19 10:52:47 +0100

    Update workflows.ts

--- common/blocs/src/data/workflows.ts
+++ common/blocs/src/data/workflows.ts
@@ -3,29 +3,6 @@ export default [
     id: "bff09ce7-a27d-4473-b632-907cbd2c4932",
     name: "Epitech",
     isActive: true,
-    action: {
-      action: {
-        id: "35a43630-ccb8-44df-8247-018ee52bb457",
-        name: "Epitech",
-      },
-    },
-    reactions: [
-      {
-        reaction: {
-          id: "388397df-64fe-48ab-b242-aa037613a2a7",
-          name: "reaction",
-        },
-      },
-    ],
-    operators: [
-      {
-        id: "644c0c05-f974-4712-b9bd-5fe3aed40258",
-        operator: {
-          id: "70e97b33-c0c4-4714-adcc-4bff5ca0b4a9",
-          name: "if 1",
-        },
-      },
-    ],
   },
   {
     id: "30d59b78-acca-4d07-a9ea-c480d9501000",
