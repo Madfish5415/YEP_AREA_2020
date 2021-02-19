8788e04eb9914168f64045806ea132212e0fd683
Mattéo Fauchon <46529475+Matttx@users.noreply.github.com>
2021-02-19 11:07:58 +0100

    Update reaction.ts

--- common/types/src/service/reaction.ts
+++ common/types/src/service/reaction.ts
@@ -3,8 +3,8 @@ import { Variable } from "./variable";
 export interface Reaction {
   readonly id: string;
   readonly name: string;
-  readonly description?: string;
-  readonly parameters?: Variable[];
+  readonly description: string;
+  readonly parameters: Variable[];
 
-  send?(parameters: Record<string, unknown>): Promise<void>;
+  send(parameters: Record<string, unknown>): Promise<void>;
 }
