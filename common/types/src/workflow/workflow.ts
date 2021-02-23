96c0ee905f5a8f692bbd30bbee5359d87bdc7a1a
Valentin Ledesert <mihobsine@macbook-pro-de-valentin.home>
2021-02-23 11:16:56 +0100

    merge: fix merge conflicts

--- common/types/src/workflow/workflow.ts
+++ common/types/src/workflow/workflow.ts
@@ -6,18 +6,9 @@ export type Workflow = {
   readonly userId: string;
   readonly id: string;
   readonly name: string;
-<<<<<<< HEAD
-  readonly isActive: boolean;
-  readonly action?: WorkflowAction;
-  readonly reactions?: WorkflowReaction[];
-  readonly operators?: WorkflowOperator[];
-  readonly runner?: Runner<Callback>;
-}
-=======
   readonly description: string;
   readonly active: boolean;
   readonly actions: WorkflowAction[];
   readonly reactions: WorkflowReaction[];
   readonly executions?: WorkflowExecution[];
 };
->>>>>>> wip/server
