2170c8307dc09ac52d59a734ef827cb4a7dbfc78
Valentin Ledesert <mihobsine@macbook-pro-de-valentin.home>
2021-03-02 15:24:30 +0100

    feat: add textfield for user information on settings page

--- web/src/components/settings/settingsInformation.tsx
+++ web/src/components/settings/settingsInformation.tsx
@@ -2,17 +2,27 @@ import React, { FC } from "react";
 import { makeStyles, Theme, Typography, Grid } from "@material-ui/core";
 import { gray, primary, white } from "@area-common/styles";
 import { User } from "@area-common/types";
+import SettingsTextInput from "./settingsTextInput";
 
 const useStyles = makeStyles((theme: Theme) => ({
   content: {
     marginLeft: 125,
     marginTop: 40,
     display: "flex",
+    flexDirection: "column",
   },
   infoTitle: {
     color: white,
-    fontSize: 30,
+    fontSize: 35,
+    marginBottom: 20,
     fontWeight: "bold",
+  },
+  userInfo: {
+    marginBottom: 20,
+  },
+  inputTitle: {
+    color: gray.light2,
+    fontSize: 30,
   },
 }));
 
@@ -24,10 +34,43 @@ type Props = {
 const SettingsInformation: FC<Props> = (props) => {
   const classes = useStyles();
 
+  const handleNameChange = (newValue: string) => {
+    props.updateUser(props.user.id, { username: newValue });
+  };
+
+  const handleFirstNameChange = (newValue: string) => {
+    props.updateUser(props.user.id, { firstName: newValue });
+  };
+
+  const handleLastNameChange = (newValue: string) => {
+    props.updateUser(props.user.id, { lastName: newValue });
+  };
+
   return (
     <>
       <div className={classes.content}>
         <Typography className={classes.infoTitle}>Information</Typography>
+        <div className={classes.userInfo}>
+          <Typography className={classes.inputTitle}>Username</Typography>
+          <SettingsTextInput
+            value={props.user.username}
+            onSubmit={handleNameChange}
+          />
+        </div>
+        <div className={classes.userInfo}>
+          <Typography className={classes.inputTitle}>First name</Typography>
+          <SettingsTextInput
+            value={props.user.firstName ? props.user.firstName : ""}
+            onSubmit={handleFirstNameChange}
+          />
+        </div>
+        <div className={classes.userInfo}>
+          <Typography className={classes.inputTitle}>Last name</Typography>
+          <SettingsTextInput
+            value={props.user.lastName ? props.user.lastName : ""}
+            onSubmit={handleLastNameChange}
+          />
+        </div>
       </div>
     </>
   );
---
+++ web/src/components/settings/settingsTextInput.tsx
@@ -0,0 +1,77 @@
+import React, { FC, useState } from "react";
+import {
+  makeStyles,
+  withStyles,
+  Theme,
+  Typography,
+  Grid,
+  TextField,
+} from "@material-ui/core";
+import { gray, primary, white } from "@area-common/styles";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  input: {
+    color: gray.light3,
+    fontSize: 25,
+  },
+}));
+
+const CssTextField = withStyles({
+  root: {
+    marginTop: 0,
+    backgroundColor: gray.light1,
+    borderRadius: 10,
+    height: 45,
+    width: 450,
+    display: "flex",
+    justifyContent: "center",
+    "& label.Mui-focused": {
+      color: gray.main,
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: gray.main,
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: gray.main,
+      },
+      "&:hover fieldset": {
+        borderColor: gray.main,
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: gray.main,
+      },
+    },
+  },
+})(TextField);
+
+type Props = {
+  value: string;
+  onSubmit: (newValue: string) => void;
+};
+
+const SettingsTextInput: FC<Props> = (props) => {
+  const classes = useStyles();
+  const [input, setInput] = useState(props.value);
+
+  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    props.onSubmit(input);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <CssTextField
+          id={"username"}
+          variant={"outlined"}
+          defaultValue={props.value}
+          InputProps={{ className: classes.input }}
+          onChange={(event) => setInput(event.target.value)}
+        />
+      </form>
+    </>
+  );
+};
+
+export default SettingsTextInput;
