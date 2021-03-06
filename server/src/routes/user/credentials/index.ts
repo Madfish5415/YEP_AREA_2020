787c3f722ada62444adb3735d9f6c3b1357868b0
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2021-03-06 13:57:50 +0100

    fix: Missing call of credential router

--- server/src/routes/user/credentials/index.ts
+++ server/src/routes/user/credentials/index.ts
@@ -2,8 +2,11 @@ import { APIResponse, User } from "@area-common/types";
 import { Router } from "express";
 
 import { USER_CREDENTIALS_ROUTE } from "../../../constants";
+import { userCredentialRouter } from "./[id]";
 
 export const userCredentialsRouter = Router();
+
+userCredentialsRouter.use(USER_CREDENTIALS_ROUTE, userCredentialRouter);
 
 userCredentialsRouter.get(USER_CREDENTIALS_ROUTE, async (req, res, next) => {
   try {
