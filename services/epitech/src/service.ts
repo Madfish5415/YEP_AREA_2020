d3e477638fe7b33af61864e9a77a87dbeff04c8c
Lucas Mathieux <lucas.mathieux@epitech.eu>
2021-03-07 18:20:17 +0100

    feat: Add nodes for epitech service

--- services/epitech/src/service.ts
+++ services/epitech/src/service.ts
@@ -1,9 +1,17 @@
-import { BaseService } from "@area-common/service";
+import {BaseService} from "@area-common/service";
+import {ActivityEndNode, ModuleEndNode, ProjectEndNode} from "./actions";
+import {ActivityRegisterNode, ModuleRegisterNode} from "./reactions";
 
 export class EpitechService extends BaseService {
   readonly id: string = "epitech";
   readonly name: string = "Epitech";
   readonly version: string = "1.0.0";
   readonly description: string = "Epitech service for AREA";
-  readonly nodes = [];
+  readonly nodes = [
+    new ActivityEndNode(),
+    new ModuleEndNode(),
+    new ProjectEndNode(),
+    new ActivityRegisterNode(),
+    new ModuleRegisterNode()
+  ];
 }
