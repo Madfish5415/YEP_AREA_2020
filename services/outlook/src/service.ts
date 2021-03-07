3fffd188f22ac79481bdc9712f0d3db0987d0204
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2021-03-07 15:23:59 +0100

    feat: Add Mail send node

--- services/outlook/src/service.ts
+++ services/outlook/src/service.ts
@@ -8,13 +8,14 @@ import {
   CLIENT_SECRET,
   TOKEN_URL,
 } from "./constants";
+import { MailSendNode } from "./reactions";
 
 export class OutlookService extends OAuth2Service {
   readonly id: string = "outlook";
   readonly name: string = "Outlook";
   readonly version: string = "1.0.0";
   readonly description: string = "Outlook service for AREA";
-  readonly nodes = [new MailNewNode()];
+  readonly nodes = [new MailNewNode(), new MailSendNode()];
   readonly options: StrategyOptions = {
     authorizationURL: AUTHORIZATION_URL,
     tokenURL: TOKEN_URL,
